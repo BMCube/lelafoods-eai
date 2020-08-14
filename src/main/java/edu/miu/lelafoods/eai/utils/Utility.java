a228e1e94a1e28222a5fe09b853eb99e55e2c74b
mamekelle <mamekelle@gmail.com>
2020-08-14 06:31:59 -0500

    :recycle: naming modification

--- src/main/java/edu/miu/lelafoods/eai/utils/Utility.java
+++ src/main/java/edu/miu/lelafoods/eai/utils/Utility.java
@@ -5,13 +5,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class Utility {
     public String cartToJson(Object object) {
         ObjectMapper mapper = new ObjectMapper();
-        String cartJson = "";
+        String ObjectToJson = "";
         try {
-            cartJson = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(object);
-            System.out.println(object.getClass().getSimpleName() + " Object to Json converted: " + cartJson);
+            ObjectToJson = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(object);
+            System.out.println(object.getClass().getSimpleName() + " Object to Json converted: " + ObjectToJson);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return cartJson;
+        return ObjectToJson;
     }
 }
